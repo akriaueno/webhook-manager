1b68dfbed33b62dd33a2b4dd068c0352a76f45df
akiraueno <akiraueno@outlook.com>
2024-08-12 02:49:07 +0900

    ci: 🎡 add sonarcloud and coverage settings

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globals: true,
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "lcov"],
+    },
   },
 });
